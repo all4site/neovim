bf7689101c193d454ec92be782433f2b5508e46e
all4site <all4sitecomua@gmail.com>
2024-12-27 20:32:02 +0000

    chande gitsings line width

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,15 @@
 return {
+
 	"lewis6991/gitsigns.nvim",
+
 	opts = {
 		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "▎" },
-			topdelete = { text = "▎" },
-			changedelete = { text = "▎" },
+			char = "│",
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "│" },
+			topdelete = { text = "│" },
+			changedelete = { text = "│" },
 			untracked = { text = "◌" },
 		},
 		current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
@@ -19,4 +22,3 @@ return {
 		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
 	},
 }
-
