647157cff97febd98989f5e34cce993b4c73d276
all4site <all4sitecomua@gmail.com>
2025-01-01 02:25:48 +0000

    add vtsls insted of ts_ls becouse ts_ls does not work with absolute path auto import

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -141,10 +141,10 @@ return {
 			})
 
 			-- Set up lspconfig.
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			require("lspconfig")["ts_ls"].setup({ capabilities = capabilities })
-			require("lspconfig")["lua_ls"].setup({ capabilities = capabilities })
-			require("lspconfig")["html"].setup({ capabilities = capabilities })
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- require("lspconfig")["ts_ls"].setup({ capabilities = capabilities })
+			-- require("lspconfig")["lua_ls"].setup({ capabilities = capabilities })
+			-- require("lspconfig")["html"].setup({ capabilities = capabilities })
 		end,
 	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,28 +1,36 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 return {
-    'neovim/nvim-lspconfig',
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.ts_ls.setup({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-            capabilities = capabilities
-        })
-        lspconfig.cssls.setup({
-            filetypes = { "css", "scss", "sass" },
-            capabilities = capabilities
-        })
-        lspconfig.cssmodules_ls.setup({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-            capabilities = capabilities
-        })
-        lspconfig.css_variables.setup({
-            filetypes = { "css", "scss", "less" },
-            capabilities = capabilities
-        })
-        lspconfig.somesass_ls.setup({
-            filetypes = { "scss", "less" },
-            capabilities = capabilities
-        })
-    end
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.vtsls.setup({
+			filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			capabilities = capabilities,
+			settings = {
+				typescript = {
+					preferences = {
+						importModuleSpecifier = "non-relative",
+						importModuleSpecifierPreference = "non-relative",
+					},
+				},
+			},
+		})
+		lspconfig.cssls.setup({
+			filetypes = { "css", "scss", "sass" },
+			capabilities = capabilities,
+		})
+		lspconfig.cssmodules_ls.setup({
+			filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			capabilities = capabilities,
+		})
+		lspconfig.css_variables.setup({
+			filetypes = { "css", "scss", "less" },
+			capabilities = capabilities,
+		})
+		lspconfig.somesass_ls.setup({
+			filetypes = { "scss", "less" },
+			capabilities = capabilities,
+		})
+	end,
 }
