089b26afac53186719b880bc3f64b186288d631b
all4site <all4sitecomua@gmail.com>
2025-07-02 01:55:26 +0300

    add bash

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,6 +11,13 @@ return {
 				typescriptreact = { "prettierd", stop_after_first = true },
 				javascript = { "prettierd", stop_after_first = true },
 				javascriptreact = { "prettierd", stop_after_first = true },
+				sh = { "shfmt" },
+				bash = { "shfmt" },
+			},
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2", "-ci" }, -- 2 пробела и авто-отступ для case/esac
+				},
 			},
 			format_on_save = {
 				lsp_fallback = true,
